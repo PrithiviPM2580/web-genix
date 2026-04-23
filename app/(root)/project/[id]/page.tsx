11c5e3be089946eb9096164864272570d23989b8
Prithivi <prithivi@users.noreply.github.com>
2026-04-24 01:41:31 +0545

    feat: implement Project component to fetch and display project details

--- app/(root)/project/[id]/page.tsx
+++ app/(root)/project/[id]/page.tsx
@@ -1,5 +1,23 @@
-import React from "react"
+"use client"
+
+import { useGetProjectById } from "@/features/use-project-id"
+import { useParams } from "next/navigation"
+import Header from "./_common/header"
 
 export default function Project() {
-  return <div>Project</div>
+  const param = useParams()
+  const id = param.id as string
+
+  const { data: project, isPending } = useGetProjectById(id)
+  const frames = project?.frames || []
+  const theme = project?.theme || ""
+
+  if (!isPending && !project) {
+    return <div>Project not found</div>
+  }
+  return (
+    <div className="relative flex h-screen w-full flex-col">
+      <Header projectName={project?.name} />
+    </div>
+  )
 }
